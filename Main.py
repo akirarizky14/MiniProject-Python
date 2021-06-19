f5d9650e34019d904345b87c17c3ad4ea3ede015
akirarizky14 <akirarizkyyy@gmail.com>
2021-06-20 00:33:27 +0300

    Hand Detecting

--- Main.py
+++ Main.py
@@ -1,15 +1,20 @@
 import cv2 
 import time
 import numpy as np
+import Hand_Tracking_Module as htm
+import time
 
 wCam ,hCam = 640,480
 capture = cv2.VideoCapture(0)
 capture.set(3,wCam)
 capture.set(4,hCam)
 pTime = 0
+detector = htm.handDetector(maxHands=1)
 
 while True:
     success, img = capture.read()
+    img = detector.findHands(img)
+    lmList, bbox = detector.findPosition(img)
 
     # Frame Rate
     cTime = time.time()
